faa306630f25ba7672fb01f45d556e4121e499fe
cym <591546487@qq.com>
2020-03-04 16:09:18 +0800

    优化回访情况取值

--- AKTWaiterCloud/Controller/MainController/ItemController/UnfinishedTask/MinuteTask/VisitCell.m
+++ AKTWaiterCloud/Controller/MainController/ItemController/UnfinishedTask/MinuteTask/VisitCell.m
@@ -21,17 +21,16 @@
     // Configure the view for the selected state
 }
 -(void)setVisitInfo:(OrderInfo *)orderinfo{
-    
     // 回访人
     NSMutableAttributedString *strName = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"回访人：%@",kString(orderinfo.visitUserName).length == 0 ? @"无":orderinfo.visitUserName] attributes:@{NSFontAttributeName: [UIFont fontWithName:@"PingFang SC" size: 13],NSForegroundColorAttributeName:kColor(@"C1")}];
        [strName addAttributes:@{NSForegroundColorAttributeName:kColor(@"C7")} range:NSMakeRange(0, 3)];
        self.namelabel.attributedText = strName;
     // 满意度
-    NSMutableAttributedString *strSatis = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"满意度：%@",kString(orderinfo.CustomerSatisfactionName).length == 0 ? @"无":orderinfo.CustomerSatisfactionName] attributes:@{NSFontAttributeName: [UIFont fontWithName:@"PingFang SC" size: 13],NSForegroundColorAttributeName:kColor(@"C1")}];
+    NSMutableAttributedString *strSatis = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"满意度：%@",kString(orderinfo.customerSatisfactionName).length == 0 ? @"无":orderinfo.customerSatisfactionName] attributes:@{NSFontAttributeName: [UIFont fontWithName:@"PingFang SC" size: 13],NSForegroundColorAttributeName:kColor(@"C1")}];
        [strSatis addAttributes:@{NSForegroundColorAttributeName:kColor(@"C7")} range:NSMakeRange(0, 3)];
        self.satisfactorylabel.attributedText = strSatis;
     // 回访时间
-    NSMutableAttributedString *strTime = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"回访时间：%@",kString(orderinfo.VisitDate).length == 0 ? @"无":orderinfo.VisitDate] attributes:@{NSFontAttributeName: [UIFont fontWithName:@"PingFang SC" size: 13],NSForegroundColorAttributeName:kColor(@"C1")}];
+    NSMutableAttributedString *strTime = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"回访时间：%@",kString(orderinfo.visitDate).length == 0 ? @"无":orderinfo.visitDate] attributes:@{NSFontAttributeName: [UIFont fontWithName:@"PingFang SC" size: 13],NSForegroundColorAttributeName:kColor(@"C1")}];
     [strTime addAttributes:@{NSForegroundColorAttributeName:kColor(@"C7")} range:NSMakeRange(0, 4)];
     self.timelabel.attributedText = strTime;
     // 回访情况
