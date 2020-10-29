5d6d877328c3627d437ec6a00cf4495781d8a16a
cym <591546487@qq.com>
2020-10-29 16:34:38 +0800

    修复签出日期与签入日期不在同一天不能签出

--- AKTWaiterCloud/Controller/UnfinishedTask/orderDetails/MinuteTaskController.m
+++ AKTWaiterCloud/Controller/UnfinishedTask/orderDetails/MinuteTaskController.m
@@ -382,7 +382,8 @@
                 
                 /**判断当前工单是否在服务有效期内**/
                 formatter.dateFormat = @"yyyy-MM-dd";
-                if ([AktUtil compareDate:[formatter dateFromString:self.orderinfo.serviceDateEnd] End:[formatter dateFromString:[AktUtil getNowDate]]] == -1) { // 超出
+//                NSLog(@"签入日期%@",[self.orderinfo.actualBegin substringToIndex:10]);
+                if ([AktUtil compareDate:[formatter dateFromString:[self.orderinfo.actualBegin substringToIndex:10]] End:[formatter dateFromString:[AktUtil getNowDate]]] != 0) { // 超出
                     [self showMessageAlertWithController:self title:@"温馨提示" Message:@"您的工单不在服务有效期内！" canelBlock:^{}];
                     return;
                 }
