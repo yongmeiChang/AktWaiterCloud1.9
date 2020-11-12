f13ad3a381bff5e9923688503470a20dda3a4e64
cym <591546487@qq.com>
2020-11-12 10:18:26 +0800

    3.0.0-1.2小版本

--- AKTWaiterCloud/Controller/AktloginVC/FindPswController.m
+++ AKTWaiterCloud/Controller/AktloginVC/FindPswController.m
@@ -2,8 +2,8 @@
 //  FindPswController.m
 //  AKTWaiterCloud
 //
-//  Created by 孙嘉斌 on 2017/10/9.
-//  Copyright © 2017年 孙嘉斌. All rights reserved.
+//  Created by 常 on 2019/10/9.
+//  Copyright © 2019年 常. All rights reserved.
 //
 
 #import "FindPswController.h"
