5f9af642f64eba959c0c2576f5dbf6630ebd6a13
cym <591546487@qq.com>
2021-05-18 17:42:56 +0800

    新增信息管理相关图片

---
+++ AKTWaiterCloud/view/vipcell/AktOldPeopleView.h
@@ -0,0 +1,17 @@
+//
+//  AktOldPeopleView.h
+//  AKTWaiterCloud
+//
+//  Created by 常永梅 on 2021/5/18.
+//  Copyright © 2021 常永梅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AktOldPeopleView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AKTWaiterCloud/view/vipcell/AktOldPeopleView.m
@@ -0,0 +1,21 @@
+//
+//  AktOldPeopleView.m
+//  AKTWaiterCloud
+//
+//  Created by 常永梅 on 2021/5/18.
+//  Copyright © 2021 常永梅. All rights reserved.
+//
+
+#import "AktOldPeopleView.h"
+
+@implementation AktOldPeopleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
